c4f18dfc494aa5ec6ea3ed70e9e9dcf7f86f4c96
gsmit592 <gsmit592@ford.com>
2021-05-03 20:38:58 +0100

    updating message

--- src/main/java/com/guitarshack/Entrypoint.java
+++ src/main/java/com/guitarshack/Entrypoint.java
@@ -5,6 +5,6 @@ import io.javalin.Javalin;
 public class Entrypoint {
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(8080);
-        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/", ctx -> ctx.result("Hello World, I have changed!"));
     }
 }
