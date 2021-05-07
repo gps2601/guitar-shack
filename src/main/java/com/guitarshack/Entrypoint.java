0e4237e2c484fd527284811dd56ed322ddd3961d
Ben Suskins <bsuskins@ford.com>
2021-05-07 16:08:29 +0100

    Hooked OrderHandler up to the EmailReoderNotifier

--- src/main/java/com/guitarshack/Entrypoint.java
+++ src/main/java/com/guitarshack/Entrypoint.java
@@ -1,12 +1,18 @@
 package com.guitarshack;
 
+import com.mailjet.client.ClientOptions;
+import com.mailjet.client.MailjetClient;
 import io.javalin.Javalin;
 
 import java.util.Date;
 
 public class Entrypoint {
     private static final RestockChecker restockChecker = RestockCheckerBuilder.build(new TwilioReorderNotifier(), Date::new);
-    private static final OrderHandler orderHandler = new OrderHandler(restockChecker);
+    private static final OrderHandler orderHandler = new OrderHandler(
+            RestockCheckerBuilder.build(
+                    new EmailReorderNotifier(
+                            new MailjetClient(System.getenv("API_KEY"), System.getenv("API_SECRET"), new ClientOptions("v3.1"))),
+                    Date::new));
 
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(Integer.parseInt(System.getenv("PORT")));
