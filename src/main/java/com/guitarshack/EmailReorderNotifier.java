85320ba81eaad21bcc25a3857eeb0c45c8ade5b0
Ben Suskins <bsuskins@ford.com>
2021-05-07 15:53:14 +0100

    Cleaned up Email Address getting

--- src/main/java/com/guitarshack/EmailReorderNotifier.java
+++ src/main/java/com/guitarshack/EmailReorderNotifier.java
@@ -2,7 +2,6 @@ package com.guitarshack;
 
 import com.mailjet.client.MailjetClient;
 import com.mailjet.client.MailjetRequest;
-import com.mailjet.client.MailjetResponse;
 import com.mailjet.client.errors.MailjetException;
 import com.mailjet.client.errors.MailjetSocketTimeoutException;
 import com.mailjet.client.resource.Emailv31;
@@ -19,15 +18,16 @@ public class EmailReorderNotifier implements ReorderNotifier {
 
     @Override
     public void send(String notification) {
+        String emailAddress = System.getenv("EMAIL_ADDRESS");
         MailjetRequest request = new MailjetRequest(Emailv31.resource)
                 .property(Emailv31.MESSAGES, new JSONArray()
                         .put(new JSONObject()
                                 .put(Emailv31.Message.FROM, new JSONObject()
-                                        .put("Email", System.getenv("EMAIL_ADDRESS"))
+                                        .put("Email", emailAddress)
                                         .put("Name", "Warehouse"))
                                 .put(Emailv31.Message.TO, new JSONArray()
                                         .put(new JSONObject()
-                                                .put("Email", System.getenv("EMAIL_ADDRESS"))
+                                                .put("Email", emailAddress)
                                                 .put("Name", "Guitar Shack Manager")))
                                 .put(Emailv31.Message.SUBJECT, "Guitar Shack Restock Notification.")
                                 .put(Emailv31.Message.TEXTPART, notification)
